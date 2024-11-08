74ec8a70160f71ed59a4cac46caf68e443679729
Simon Gomes <busy.s.simon@gmail.com>
2024-11-08 23:00:02 +0800

    chore: tailwind theme config added

--- app/index.jsx
+++ app/index.jsx
@@ -3,8 +3,8 @@ import { Link } from "expo-router";
 
 const App = () => {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-2xl text-black">React Native CC</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl text-black">React Native CC</Text>
       <Link href="/profile" style={{color: 'blue'}}>Profile</Link>
     </View>
   );
